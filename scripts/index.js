487b4531090ff24737006f85d2fc44ec5529ea53
AliBabaei-i <155062135+AliBabaei-i@users.noreply.github.com>
2023-12-31 11:22:27 +0330

    Delete scripts directory

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-const fullStorage = document.getElementById("freeStorage");
-const freeStorage = document.getElementById("first");
-const range = document.querySelector(".data");
-
-range.addEventListener("input", () => {
-    const value = range.value;
-    fullStorage.textContent = `${value} GB`;
-    freeStorage.textContent = 1000 - value;
-});
